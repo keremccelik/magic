666befa981f8471022660eb0f1d1b5f2bbcfac36
keremccelik <keremccelik@gmail.com>
2024-02-02 15:58:49 +0300

    commit

---
+++ test.js
@@ -0,0 +1,52 @@
+<picture>
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 1920px)"
+    type="image/webp"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 1920px)"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 1024px), (min-device-pixel-ratio: 2)"
+    type="image/webp"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 1024px), (min-device-pixel-ratio: 2)"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 1280px)"
+    type="image/webp"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 1280px)"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 640px)"
+    type="image/webp"
+  />
+  <source
+    srcset="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    media="(min-width: 640px)"
+  />
+  <img
+    src="https://uploads-ssl.webflow.com/657fefc732f7337cad6c4137/65ba5774ec68902bdac9775b_Diadora%20Made%20in%20Italy.jpg"
+    alt="DIADORA | Made in Italy"
+  />
+</picture>;
+<video class="video" muted="" loop="" playsinline="" preload="auto">
+  <source
+    src="https://s3.amazonaws.com/webflow-prod-assets/657fefc732f7337cad6c4137/65ba62dedee6e5b230f05cae_Diadora%20%20Made%20in%20Italy.mp4"
+    type="video/webm"
+  />
+  <source
+    src="https://s3.amazonaws.com/webflow-prod-assets/657fefc732f7337cad6c4137/65ba62dedee6e5b230f05cae_Diadora%20%20Made%20in%20Italy.mp4"
+    type="video/mp4"
+  />
+</video>;
